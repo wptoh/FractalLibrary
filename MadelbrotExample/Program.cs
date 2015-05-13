5bbeacecb9a1cfba3e141629ac01e1b76904f462
weiping <workaholic.angel.86@gmail.com>
2015-05-13 18:28:13 +0800

    Test Done

--- MadelbrotExample/Program.cs
+++ MadelbrotExample/Program.cs
@@ -29,6 +29,32 @@ namespace MadelbrotExample
 			//throw new NotImplementedException ();
 		}
 	}
+
+	public class QuadraticJulietIterator : MandelbrotIterator
+	{
+		public override void Iterate (params object[] arguments)
+		{
+			FractalComplexNumber z = new FractalComplexNumber ();
+			FractalComplexNumber[] complexNos = (FractalComplexNumber[])arguments [1];
+			int iterations = (int)arguments [0];
+			if (complexNos.Length > 1)
+			{
+				z = complexNos [1];
+			}
+			FractalComplexNumber c = complexNos [0];
+			for (int i = 0; i < iterations + 1; ++i)
+			{
+				z = z * z + c;
+				if (z.Absolute > 2f)
+				{
+					ReturnValue = i;
+					break;
+				}
+			}
+			//throw new NotImplementedException ();
+		}
+	}
+
 	class MainClass
 	{
 		public static void Main (string[] args)
