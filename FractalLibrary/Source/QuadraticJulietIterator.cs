e05e50a45ea0dc417aa76e295cdd9ce45ec1b1e0
pws dev <pws.dev.tools@gmail.com>
2015-05-19 15:24:34 +0800

    Included the standard Mandelbrot Iterators as part of the main library

---
+++ FractalLibrary/Source/QuadraticJulietIterator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FractalLibrary
+{
+	public class QuadraticJulietIterator : MandelbrotIterator
+	{
+		public override void Iterate (params object[] arguments)
+		{
+			FractalComplexNumber z = new FractalComplexNumber ();
+			FractalComplexNumber[] complexNos = (FractalComplexNumber[])arguments [1];
+			int iterations = (int)arguments [0];
+			if (complexNos.Length > 1)
+			{
+				z = complexNos [1];
+			}
+			FractalComplexNumber c = complexNos [0];
+			for (int i = 0; i < iterations + 1; ++i)
+			{
+				z = z * z + c;
+				if (z.Absolute > 2f)
+				{
+					ReturnValue = i;
+					break;
+				}
+			}
+			//throw new NotImplementedException ();
+		}
+	}
+}
+
